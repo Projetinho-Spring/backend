9e4835155e734f6de76b86185bb26907206f8c52
4mxi <mxirocha3@gmail.com>
2026-02-27 19:43:08 -0300

    feat: Seta createdAt pra gerar automaticamente ao criar user

--- src/main/java/br/com/fatec/teste/user/UserService.java
+++ src/main/java/br/com/fatec/teste/user/UserService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.time.LocalDateTime;
 
 @Service
 public class UserService {
@@ -15,6 +16,7 @@ public class UserService {
     }
 
     public User create(User user) {
+        user.setCreatedAt(LocalDateTime.now());
         return repository.save(user);
     }
 
